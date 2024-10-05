d071ae4e8d1278705f0ee61ff41622aa2896fc6d
Noa <lqian062@gmail.com>
2024-10-05 18:25:57 +0900

    カーブ移動のスクリプト完了

--- Assets/Script/Uchida/OtherEnemyScript.cs
+++ Assets/Script/Uchida/OtherEnemyScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class OtherEnemyScript : MonoBehaviour
 {
     public float MoveSpeed=3.0f;
+    public float UpDownSpeed = 5.0f;
+    private bool UpDownBool = false;//false‚¾‚Æ‰º‚És‚­
 
     void Start()
     {
@@ -20,9 +22,13 @@ public class OtherEnemyScript : MonoBehaviour
         this.transform.position -= MoveSpeed * Time.deltaTime * transform.right;
         Vector2 MyTransform = this.transform.position;
         if (MyTransform.y >= 3.5f)
-            transform.position -= MoveSpeed * Time.deltaTime * transform.up;
-        if (MyTransform.y <= -3.5f)
-            transform.position += MoveSpeed * Time.deltaTime * transform.up;
+            UpDownBool = false;
+        if(MyTransform.y <= -3.5f)
+            UpDownBool=true;
+        if (UpDownBool==false)
+            transform.position -= UpDownSpeed * Time.deltaTime * transform.up;
+        if (UpDownBool==true)
+            transform.position += UpDownSpeed * Time.deltaTime * transform.up;
     }
 
 }//end
